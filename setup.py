38e63e853bc9e0c2df75374aa871283ec2804ec1
Pierre Jaury <pierre@jaury.eu>
2013-05-03 11:56:30 +0200

    Add a setup configuration

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""
+Setup configuration.
+"""
+
+import setuptools
+
+
+setuptools.setup(
+    name="twistmc",
+    version="1.0",
+    author="Pierre Jaury",
+    description="Twist My Components!",
+    license="GPLv3",
+    py_modules=['twistmc'],
+)
