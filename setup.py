dbb461c8c62f35da9a2fff07e5c514322259e706
Pierre Jaury <pierre@jaury.eu>
2015-05-03 16:46:23 +0200

    Update the setup file

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-"""
-Setup configuration.
-"""
-
 import setuptools
-
 
 setuptools.setup(
     name="twistmc",
-    version="1.0",
+    version="2.0",
     author="Pierre Jaury",
+    email="pierre@jaury.eu",
     description="Twist My Components!",
+    url="https://github.com/kaiyou/twistmc.git",
     license="GPLv3",
-    py_modules=['twistmc'],
+    py_modules=["twistmc"],
+    install_requires=[
+        "pyconsts >= 1.0"
+    ]
 )
